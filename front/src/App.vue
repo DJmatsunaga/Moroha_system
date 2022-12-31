b2d046ba80380788b63cdf75582a44e4566249ae
DJmatsunaga <naritari0616@icloud.com>
2023-01-01 00:51:04 +0900

    トップページ作成

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,23 +1,42 @@
 <template>
-  <img alt="MorohaLogo" src="./assets/cheers.jpeg" />
-  <Index></Index>
-  <div>競合解消中</div>
+  <main class="app-content">
+    <div class="app-content-title">
+      <img
+        class="app-content-title-image"
+        alt="MorohaLogo"
+        src="./assets/cheers.jpeg"
+      />
+      <div class="app-content-sub-title">〜刹那の乾杯〜</div>
+    </div>
+    <div class="app-content-button">
+      <Button
+        label="幕開けの夜"
+        class="p-button-secondary p-button-text p-button-lg"
+      ></Button>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Index from "./components/index.vue";
+// import Index from "./components/index.vue";
 
 export default defineComponent({
   name: "App",
-  components: {
-    Index,
-  },
 });
 </script>
 
 <style>
-html {
+.app-content {
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.app-content-sub-title {
+  text-align: center;
+  color: white;
+  font-size: 36px;
 }
 </style>
--- front/src/main.ts
+++ front/src/main.ts
@@ -1,4 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
 
-createApp(App).mount("#app");
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import "primevue/resources/themes/luna-blue/theme.css";
+
+const app = createApp(App);
+app.use(PrimeVue).component("Button", Button);
+app.mount("#app");
